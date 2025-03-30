bda6c97f54f6d9aeb45a7a9429c85fc9a7922baf
Tomek515 <149333002+Tomek515@users.noreply.github.com>
2025-03-30 14:26:41 +0200

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,100 +1,131 @@
 #include <iostream>
-int menuglowne;
+#include <vector>
 
-std::string numer;
+using namespace std;
 
- class nowykontakt
-{
-  public:
-  std::string imie;
-  std::string nazwisko;
-  int telnr;
+class Kalkulator {
+private:
+    vector<int> v1, v2;
+    int dlugoscwyniku;
 
+public:
+    Kalkulator(int rozmiar1, int rozmiar2) {
+        cout << "Podaj liczby dla pierwszego wektora:\n";
+        v1 = wczytaj_wektor(rozmiar1);
+        cout << "Podaj liczby dla drugiego wektora:\n";
+        v2 = wczytaj_wektor(rozmiar2);
+    }
 
-  
+    void wyrownaj_wektory() {
+        int roznica = v1.size() - v2.size();
+        if (roznica > 0) {
+            v2.insert(v2.begin(), roznica, 0);
+        } else if (roznica < 0) {
+            v1.insert(v1.begin(), -roznica, 0);
+        }
+        dlugoscwyniku = v1.size();
+    }
+
+    vector<int> dodawanie() {
+        wyrownaj_wektory();
+        vector<int> wynik(dlugoscwyniku);
+        int przeniesienie = 0;
+
+        for (int i = dlugoscwyniku - 1; i >= 0; i--) {
+            int suma = v1[i] + v2[i] + przeniesienie;
+            wynik[i] = suma % 10;
+            przeniesienie = suma / 10;
+        }
+
+        if (przeniesienie > 0) {
+            wynik.insert(wynik.begin(), przeniesienie);
+        }
+
+        return wynik;
+    }
+
+    vector<int> odejmowanie() {
+        wyrownaj_wektory();
+        vector<int> wynik(dlugoscwyniku);
+        int przeniesienie = 0;
+        bool ujemny = false;
+        if(v1<v2){
+            swap(v1, v2);
+            ujemny = true;
+        }
+        for (int i = dlugoscwyniku - 1; i >= 0; i--) {
+            int roznica = v1[i] - v2[i] - przeniesienie;
+            if (roznica < 0) {
+                roznica += 10;
+                przeniesienie = 1;
+            } else {
+                przeniesienie = 0;
+            }
+            wynik[i] = roznica;
+        }
+
+        // Usunięcie zer
+        while (wynik.size() > 1 && wynik[0] == 0) {
+            wynik.erase(wynik.begin());
+        }
+        if (ujemny) {
+            wynik[0] *= -1;
+        }
+        return wynik;
+    }
+
+    static vector<int> wczytaj_wektor(int rozmiar) {
+        vector<int> wektor(rozmiar);
+        cout << "Podaj liczbe (cyfry bez spacji, np. 12345): ";
+        string liczba;
+        cin >> liczba;
+
+        for (int i = 0; i < rozmiar; i++) {
+            wektor[i] = liczba[i] - '0';
+        }
+
+        return wektor;
+    }
+
+    static void wypisz_wektor(const vector<int>& wektor) {
+        for (int val : wektor) {
+            cout << val;
+        }
+        cout << endl;
+    }
 };
 
-   void menu(){
-    std::cout << "1.Połączenia" << std::endl;
- std::cout << "2.Kontakty" << std::endl;
- std::cout << "3.SMS" << std::endl;
- 
-   }
-  void wybieranienumeru(){
-     system( "cls" );
-    std::cout << "wprowadz numer, wciśnij dwa aby wrócić do menu" << std::endl;  
-    //std::string numer;
+int main() {
+    int rozmiar1, rozmiar2;
+    cout << "Podaj dlugosc pierwszej liczby: ";
+    cin >> rozmiar1;
+    cout << "Podaj dlugosc drugiej liczby: ";
+    cin >> rozmiar2;
 
-    std::cin>> numer;
-    if (numer.length()==9)//wprowadzenie poprawnego numeru 
-    {
-      std::cout << "Dzwonienie" << numer << std::endl; 
-      std::cout << "Wciśnij Enter zeby przerwać" << std::endl; 
-      getchar();
-      getchar();
-      run();
+    Kalkulator kalkulator(rozmiar1, rozmiar2);
+
+    cout << "\nWybierz operację:\n";
+    cout << "1. Dodawanie (+)\n";
+    cout << "2. Odejmowanie (-)\n";
+    int wybor;
+    cin >> wybor;
+
+    switch (wybor) {
+        case 1: {
+            vector<int> wynik = kalkulator.dodawanie();
+            cout << "\nWynik dodawania: ";
+            Kalkulator::wypisz_wektor(wynik);
+            break;
+        }
+        case 2: {
+            vector<int> wynik = kalkulator.odejmowanie();
+            cout << "\nWynik odejmowania: ";
+            Kalkulator::wypisz_wektor(wynik);
+            break;
+        }
+        default:
+            cout << "Zly wybor!" << endl;
     }
-    else if(numer=="2"){// opuszczenie menu wybierania numeru
-      run();
-    }
-    else {  //wprowadzenie niepoprawnegu numeru, ponowne wybieranie lub powrót do menu
-                    std::cout << "Numer musi składać się z 9 cyfr" << std::endl; 
-                    std::cout << "wprowadz numer, wciśnij dwa aby wrócić do menu" << std::endl;  
-                    
-                    std::cin>> numer;
-                    if (numer.length()==9)
-                    {
-                      std::cout << "Dzwonienie" << numer << std::endl; 
-                      std::cout << "Wciśnij Enter zeby przerwać" << std::endl; 
-                      getchar();
-                      getchar();
-                      run();
-                    }
-                      else if(numer=="2"){
-                      run();
-                      }
-                    else {
-                    std::cout << "Numer musi składać się z 9 cyfr" << std::endl; 
-                    std::cout << "wprowadz numer, wciśnij dwa aby wrócić do menu" << std::endl;  
-      
-      
-  
-    }
-  }
-  }
 
- void run(){
-    
-    menu();
-    std::cin >> menuglowne;
-
-     switch(menuglowne){
-    case 1: // wybieranie numeru
-   wybieranienumeru();
-    break;
-
-    case 2: // wybieranie sms
-    system( "cls" );
-    std::cout << "wprowadz numer" << std::endl;  
-    break;
-
-    case 3: //t ksiażka telefoniczna 
-    system( "cls" );
-    std::cout << "wprowadz numer" << std::endl;  
-    break;
-   }
- }
-int main (){
-    
-//run();
-nowykontakt kontakt1;
-kontakt1.imie = "tomasz";
-kontakt1.nazwisko= "morawa";
-kontakt1.telnr = 123456789;
-
-
- return 0;
- } 
-   
-
- 
+    return 0;
+}
